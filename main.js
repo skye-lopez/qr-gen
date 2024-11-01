0797a55aaf51109c2af35bb39070970add4568ff
skye-lopez <sky.lopez.dev@gmail.com>
2024-10-31 21:45:33 -0700

    Added method to convert content into Uint8Array

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import sizesByCorrectionLevel from "./sizetables.js"
 // FIXME: TESTING VALUES 
-const TEST_STRING = "https://www.google.com";
+const TEST_STRING = "https://www.qrcode.com/";
 const ERROR_LEVEL = "M";
 // --------------------
 
@@ -27,13 +27,13 @@ function determineEncodingType(string) {
     * Determines the min level to build based on the requested error correction level and string size.
     * @param {string} string - The data to be encoded 
     * @param {errorCorrectionLevel} - The correction level requested 
-    * @return {number} the level to encode 
+    * @return {number[]} a touple: [version, max] - version= the version of encoding, max = the size of the block. 
 */
 function determineMinVersionByErrorRate(string, errorCorrectionLevel) {
     const len = string.length;
     const sizes = sizesByCorrectionLevel[errorCorrectionLevel];
-    for (const [level, max] of Object.entries(sizes)) {
-        if (len <= max) return level;
+    for (const [version, max] of Object.entries(sizes)) {
+        if (len <= max) return [version, max];
     }
 }
 
@@ -54,4 +54,40 @@ function getBitsLength(encodingType, version) {
     };
     const versionIdx = version > 26 ? 2 : version > 9 ? 1 : 0;
     return bitLengthByEncodingAndVersion[encodingType][versionIdx];
+}
+
+/**
+    * converts the given string to a Uint8Array of bytes encoded in the correct format by the provided verison.
+    * @param {string} string - The data to encode 
+    * @param {number} bitLength - The bitlength for the version by encoding type. 
+    * @param {number} dataSize - the amount of data to encode, also determined by the version and error rate 
+    * @return {Uint8Array[]} Uint8Array of the converted data 
+*/
+function convertStringToBytes(string, bitLength, dataSize) {
+    const data = new Uint8Array(dataSize);
+    const rightShift = (4 + bitLength) & 7;
+    const leftShift = (8 - rightShift);
+    const andMask = (1 << rightShift) - 1;
+    const dataIndexStart = bitLength > 12 ? 2 : 1;
+    const len = string.length;
+
+    data[0] = 64 + (len >> (bitLength - 4));
+    if (bitLength > 12) {
+        data[1] = (len >> rightShift) && 255;
+    }
+    data[dataIndexStart] = (len & andMask) << leftShift;
+
+    for (let i = 0; i < len; i++) {
+        const byte = string.charCodeAt(i);
+        data[i + dataIndexStart] |= byte >> rightShift;
+        data[i + dataIndexStart + 1] = (byte & andMask) << leftShift;
+    }
+
+    const remainder = dataSize - len - dataIndexStart - 1;
+    for (let i = 0; i < remainder; i++) {
+        const byte = i & 1 ? 17 : 236;
+        data[i + len + 2] = byte;
+    }
+
+    return data;
 }
